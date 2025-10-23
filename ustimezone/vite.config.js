80446aec37f6cbb56b2e689b248e313ce68571b1
Mahigavale <maheshgavale07@gmail.com>
2025-10-23 23:50:17 +0530

    V2

--- ustimezone/vite.config.js
+++ ustimezone/vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./",       // ensures relative paths work on Vercel
+  base: "./",       // relative paths for Vercel
   build: {
-    outDir: "dist", // Vercel expects this folder by default
+    outDir: "dist", // output folder Vercel expects
   },
 })
